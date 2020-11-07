c1840ade7d67de4fa16112f891f1db9e3a2a6a63
Nguyen Gia Huy <nghuycode@gmail.com>
2020-11-07 11:27:34 +0700

    Update raw demo

---
+++ SpendBillGateMoney-Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e2b81cf6aba719ccd24cd648387522a707f9ab6e2edb6b8752b1a021f7102ef
+size 536
---
+++ SpendBillGateMoney-Unity/Assets/Scripts/Item.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f5e96c2821112c842530715aa1b16d82262f43e56339b682420672c470482fb
+size 1535
